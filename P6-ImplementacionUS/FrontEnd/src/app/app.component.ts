afbc90a7fde9a1c8c59ea60478ce4edeb5998910
NicolasUrza <88108043+NicolasUrza@users.noreply.github.com>
2023-09-04 05:36:36 -0300

    tema claro

--- P6-ImplementacionUS/FrontEnd/src/app/app.component.ts
+++ P6-ImplementacionUS/FrontEnd/src/app/app.component.ts
@@ -13,10 +13,12 @@ export class AppComponent {
   entradaIzquierda = false;
   entradaDerecha = false;
   vistaActual = 0;
-  fileSelected = "Puedes incluir una imagen si quieres para que te ayudemos a encontrarlo más rápido";
+  metodoPago = "Efectivo";
+  imagenSubida = "";
+  submited = false;
   pedidoForm = new FormGroup({
     objetos: new FormControl('', [Validators.required, Validators.maxLength(1200)]),
-    imagen: new FormControl(null)
+    imagen: new FormControl('')
   });
   localForm = new FormGroup({
     calle: new FormControl('', [Validators.required, Validators.maxLength(120)]),
@@ -24,44 +26,65 @@ export class AppComponent {
     ciudad: new FormControl('', [Validators.required, Validators.maxLength(120)]),
     referencia: new FormControl('', [Validators.required, Validators.maxLength(120)])
   });
+  lugarEntregaForm = new FormGroup({
+    calle: new FormControl('', [Validators.required, Validators.maxLength(120)]),
+    numero: new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,7}')]),
+    ciudad: new FormControl('', [Validators.required, Validators.maxLength(120)]),
+    referencia: new FormControl('', [Validators.required, Validators.maxLength(120)]),
+    entrega: new FormControl('lo-antes-posible', [Validators.required, Validators.maxLength(120)]),
+    fecha: new FormControl('', [Validators.required, Validators.maxLength(120)]),
+    hora: new FormControl('', [Validators.required, Validators.maxLength(120)])
+  });
+  tarjetaForm = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.maxLength(120)]),
+    numero: new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,7}')]),
+    vencimiento: new FormControl('', [Validators.required, Validators.maxLength(120)]),
+    codigo: new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,3}')]),
+  });
 
+  efectivoForm = new FormGroup({
+    monto: new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,7}')]),
+  });
+  ConseguirFechaActual() {
+    var fecha = new Date();
+    var dia = fecha.getDate();
+    var mes = fecha.getMonth() + 1;
+    var anio = fecha.getFullYear();
+    if (dia < 10) {
+      dia = 0 + dia;
+    }
+    if (mes < 10) {
+      mes = 0 + mes;
+    }
 
-
+    return dia + "/" + mes + "/" + anio;
+  }
   CambiarVista(vista: number) {
     this.salidaIzquierda = true;
+    this.submited = false;
+    if (this.pedidoForm.controls["imagen"].value != '') {
+      this.imagenSubida = this.pedidoForm.controls["imagen"].value!;
+      this.pedidoForm.controls["imagen"].setValue('');
+    }
     setTimeout(() => {
       this.salidaIzquierda = false;
       this.vistaActual = vista;
     }, 500);
+
   }
   ExisteImagen() {
-    console.log(this.pedidoForm.controls["imagen"].value);
-    return this.pedidoForm.controls["imagen"].value != null;
+    return this.pedidoForm.controls["imagen"].value != '' || this.imagenSubida != '';
   }
-  constructor(private el: ElementRef) { }
+  constructor() { }
 
-  ngAfterViewInit() {
-    const inputs = this.el.nativeElement.querySelectorAll('.inputfile');
-    Array.prototype.forEach.call(inputs, (input: HTMLInputElement) => {
-      const label = input.nextElementSibling as HTMLElement;
-      const labelVal = label.innerHTML;
-
-      input.addEventListener('change', (e: Event) => {
-        const target = e.target as HTMLInputElement;
-        let fileName = '';
-
-        if (target.files && target.files.length > 1) {
-          fileName = (target.getAttribute('data-multiple-caption') || '').replace('{count}', target.files.length.toString());
-        } else {
-          fileName = target.value.split('\\').pop() || '';
-        }
-
-        if (fileName) {
-          label.querySelector('span')!.innerHTML = fileName;
-        } else {
-          label.innerHTML = labelVal;
-        }
-      });
-    });
+  TotalPago() {
+    return 1000;
+  }
+  MetodoDePago(metodo: string) {
+    this.metodoPago = metodo;
+    this.CambiarVista(5);
+  }
+  TieneErrores(form: FormGroup , control: string) {
+    return form.controls[control].invalid && (form.controls[control].dirty || form.controls[control].touched || this.submited)  ; 
   }
 }
