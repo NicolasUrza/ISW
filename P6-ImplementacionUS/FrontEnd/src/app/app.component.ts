f23d03c65ca392f670159d3775892ad95e6c5b7e
NicolasUrza <88108043+NicolasUrza@users.noreply.github.com>
2023-09-09 07:09:57 -0300

    limitacion de fechas y cambio de colores

--- P6-ImplementacionUS/FrontEnd/src/app/app.component.ts
+++ P6-ImplementacionUS/FrontEnd/src/app/app.component.ts
@@ -16,31 +16,31 @@ export class AppComponent {
   metodoPago = "Efectivo";
   imagenSubida = "";
   submited = false;
-  flipped=false;
-  footer=false;
-  hastaCompletado=0;
+  flipped = false;
+  footer = false;
+  hastaCompletado = 0;
 
-  HastaCompletado(){
+  HastaCompletado() {
     let hastaCompletado = 0;
-    if(this.pedidoForm.invalid && this.hastaCompletado>= 1){
-      hastaCompletado=1;
+    if (this.pedidoForm.invalid && this.hastaCompletado >= 1) {
+      hastaCompletado = 1;
     }
-    else if(this.localForm.invalid && this.hastaCompletado>= 2){
-      hastaCompletado=2;
+    else if (this.localForm.invalid && this.hastaCompletado >= 2) {
+      hastaCompletado = 2;
     }
-    else if(this.lugarEntregaForm.invalid && this.hastaCompletado>= 3){
-      hastaCompletado=3;
-    }else if((this.metodoPago=="Tarjeta" && this.tarjetaForm.invalid || this.metodoPago=="Efectivo" && this.efectivoForm.invalid) && this.hastaCompletado>= 5){
-      hastaCompletado=5;
+    else if (this.lugarEntregaForm.invalid && this.hastaCompletado >= 3) {
+      hastaCompletado = 3;
+    } else if ((this.metodoPago == "Tarjeta" && this.tarjetaForm.invalid || this.metodoPago == "Efectivo" && this.efectivoForm.invalid) && this.hastaCompletado >= 5) {
+      hastaCompletado = 5;
     }
-    else if((this.metodoPago=="Tarjeta" && this.tarjetaForm.invalid || this.metodoPago=="Efectivo" && this.efectivoForm.invalid) && this.hastaCompletado>= 4){
-      hastaCompletado=4;
+    else if ((this.metodoPago == "Tarjeta" && this.tarjetaForm.invalid || this.metodoPago == "Efectivo" && this.efectivoForm.invalid) && this.hastaCompletado >= 4) {
+      hastaCompletado = 4;
     }
-    else if(this.hastaCompletado> this.vistaActual){
-      hastaCompletado=this.hastaCompletado;
+    else if (this.hastaCompletado > this.vistaActual) {
+      hastaCompletado = this.hastaCompletado;
     }
-    else{
-      hastaCompletado=this.vistaActual;
+    else {
+      hastaCompletado = this.vistaActual;
     }
     return hastaCompletado;
   }
@@ -52,21 +52,21 @@ export class AppComponent {
     calle: new FormControl('', [Validators.required, Validators.maxLength(120)]),
     numero: new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,7}')]),
     ciudad: new FormControl('', [Validators.required, Validators.maxLength(120)]),
-    referencia: new FormControl('', [ Validators.maxLength(120)])
+    referencia: new FormControl('', [Validators.maxLength(120)])
   });
   lugarEntregaForm = new FormGroup({
     calle: new FormControl('', [Validators.required, Validators.maxLength(120)]),
     numero: new FormControl('', [Validators.required, Validators.pattern('[0-9]{1,7}')]),
     ciudad: new FormControl('', [Validators.required, Validators.maxLength(120)]),
-    referencia: new FormControl('', [ Validators.maxLength(120)]),
-    entrega: new FormControl('lo-antes-posible', [ Validators.maxLength(120)]),
-    fecha: new FormControl('', []),
+    referencia: new FormControl('', [Validators.maxLength(120)]),
+    entrega: new FormControl('lo-antes-posible', [Validators.maxLength(120)]),
+    fecha: new FormControl(this.ConseguirFechaActual(), []),
     hora: new FormControl('', [])
   });
   tarjetaForm = new FormGroup({
     nombre: new FormControl('', [Validators.required, Validators.maxLength(40)]),
     numero: new FormControl('', [Validators.required, Validators.pattern('[0-9]{16,16}'), this.isVisa()]),
-    vencimiento: new FormControl('', [Validators.required, Validators.maxLength(120)]),
+    vencimiento: new FormControl("", [Validators.required, Validators.maxLength(120)]),
     codigo: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3,3}')]),
   });
 
@@ -76,21 +76,25 @@ export class AppComponent {
   ConseguirFechaActual() {
     var fecha = new Date();
     var dia = fecha.getDate();
+    let diaString = dia.toString();
     var mes = fecha.getMonth() + 1;
+    let mesString = mes.toString(); 
     var anio = fecha.getFullYear();
     if (dia < 10) {
-      dia = 0 + dia;
+      diaString = "0" + diaString;
+
     }
     if (mes < 10) {
-      mes = 0 + mes;
-    }
-
-    return dia + "/" + mes + "/" + anio;
+      mesString = "0" + mesString;
+        }
+    let fe = anio + "-" + mesString + "-" + diaString;
+    console.log(fe);
+    return fe;
   }
   CambiarVista(vista: number) {
     this.salidaIzquierda = true;
     this.submited = false;
-    if(vista> this.hastaCompletado){
+    if (vista > this.hastaCompletado) {
       this.hastaCompletado = vista;
     }
     if (this.pedidoForm.controls["imagen"].value != '') {
@@ -100,7 +104,7 @@ export class AppComponent {
     setTimeout(() => {
       this.salidaIzquierda = false;
       this.vistaActual = vista;
-     
+
     }, 500);
 
   }
@@ -120,76 +124,88 @@ export class AppComponent {
     this.CambiarVista(5);
   }
   TieneErrores(form: FormGroup, control: string) {
-    console.log(control)
     return form.controls[control].invalid && (form.controls[control].dirty || form.controls[control].touched || this.submited);
   }
 
   Error(form: FormGroup, control: string, error: string) {
     return this.TieneErrores(form, control) && form.controls[control].hasError(error)
   }
-  QuitarRequired(){
+  QuitarRequired() {
     this.lugarEntregaForm.controls["fecha"].clearValidators();
     this.lugarEntregaForm.controls["fecha"].updateValueAndValidity();
     this.lugarEntregaForm.controls["hora"].clearValidators();
     this.lugarEntregaForm.controls["hora"].updateValueAndValidity();
 
   }
-  AgregarRequired(){
+  AgregarRequired() {
     this.lugarEntregaForm.controls["fecha"].setValidators([Validators.required]);
     this.lugarEntregaForm.controls["fecha"].updateValueAndValidity();
     this.lugarEntregaForm.controls["hora"].setValidators([Validators.required]);
     this.lugarEntregaForm.controls["hora"].updateValueAndValidity();
   }
-  NumeroTarjeta(numeroIngresado:string){
-    let numero:string = "";
-    for(let i= 0 ; i<16; i++ ){
-      if(i%4==0 && i !== 0){
+  NumeroTarjeta(numeroIngresado: string) {
+    let numero: string = "";
+    for (let i = 0; i < 16; i++) {
+      if (i % 4 == 0 && i !== 0) {
         numero += "  ";
       }
-      if(numeroIngresado.toString().length>i){
+      if (numeroIngresado.toString().length > i) {
         numero += numeroIngresado.toString()[i].toString();
       }
-      else{
+      else {
         numero += "X";
       }
-      
+
     }
     return numero;
   }
 
-  Vencimiento(){
+  Vencimiento() {
     let vencimiento = this.tarjetaForm.controls["vencimiento"].value;
-    let mes = vencimiento!.substring(0,2);
-    let anio = vencimiento!.substring(2,4);
-    return mes+"/"+anio;
+    let mes = vencimiento!.substring(0, 2);
+    let anio = vencimiento!.substring(2, 4);
+    return mes + "/" + anio;
   }
 
-  Visa(){
-    return this.tarjetaForm.controls["numero"].value!.toString()[0]=="4";
+  Visa() {
+    return this.tarjetaForm.controls["numero"].value!.toString()[0] == "4";
   }
-  MasterCard(){
-    return this.tarjetaForm.controls["numero"].value!.toString()[0]=="5";
+  MasterCard() {
+    return this.tarjetaForm.controls["numero"].value!.toString()[0] == "5";
   }
 
   isVisa(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       return this.isVisaCard(control.value.toString()) ? null : { isvisa: { value: control.value } };
-    };}
+    };
+  }
 
-    isVisaCard(cardNumber: string): boolean {
-      if (cardNumber) {
-        // que empieze con 4
-        if (cardNumber.length ==0) {
-          return false;
-        }
-        
-        if (cardNumber[0] !== '4') {
-          return false;
-        }
-        else{
-          return true;
-        }
+  isVisaCard(cardNumber: string): boolean {
+    if (cardNumber) {
+      // que empieze con 4
+      if (cardNumber.length == 0) {
+        return false;
       }
-      return false;
+
+      if (cardNumber[0] !== '4') {
+        return false;
+      }
+      else {
+        return true;
+      }
     }
+    return false;
+  }
+
+  MesyAnioActual() {
+    let fecha = new Date();
+    let mes = fecha.getMonth() + 1;
+    let anio = fecha.getFullYear().toString();
+    let mesString = mes.toString();
+    if (mes < 10) {
+      mesString = "0" + mesString;
+    }
+    return anio + "-" + mesString;
+  }
+
 }
